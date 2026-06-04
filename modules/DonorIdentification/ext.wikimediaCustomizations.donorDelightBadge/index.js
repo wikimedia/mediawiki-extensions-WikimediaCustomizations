09285ae827ca0914149bfef12c4b9e446ef199cc
Volker E. <volker.e@wikimedia.org>
2026-06-04 04:11:32 -0700

    Donor Delight Badge: Remove compatibility fix for cache
    Follow-up to I38df62d5a8c85f61ff237a7bc909e930717d6c04
    
    Depends-On: I274265a78c363a4177efb0d8ef208ffb0d53d15a
    Change-Id: I7971e2e49bc6c378abd6f701098d360edbf820b4

--- modules/DonorIdentification/ext.wikimediaCustomizations.donorDelightBadge/index.js
+++ modules/DonorIdentification/ext.wikimediaCustomizations.donorDelightBadge/index.js
@@ -61,15 +61,6 @@ function init() {
 	let minervaBadgePref = mw.user.clientPrefs.get( 'minerva-badge' );
 	const isFirstVisit = minervaBadgePref === '0';
 
-	// This module has been loaded in an error state. We will later remove this
-	// and ensure the module has not been loaded at all
-	// FIXME: This can be removed once I38df62d5a8c85f61ff237a7bc909e930717d6c04 has
-	// been in production for 2 weeks.
-	if ( document.documentElement.classList.contains( 'wikimedia-donor-badge-' ) ) {
-		mw.user.clientPrefs.set( 'minerva-badge', '0' );
-		return;
-	}
-
 	const badge = document.getElementById( 'minerva-badge' );
 	if ( !badge ) {
 		return;
