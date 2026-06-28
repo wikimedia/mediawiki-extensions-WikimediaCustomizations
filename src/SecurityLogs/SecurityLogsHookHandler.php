462c1d2c4e77c00287a40f12b7c7b8db82166aa7
Gergő Tisza <tgr.huwiki@gmail.com>
2026-06-28 21:10:30 +0200

    SecurityLogs: Create by moving code from mediawiki-config
    Moves the GetSecurityLogContext, AuthManagerLoginAuthenticateAudit
    and ChangeAuthenticationDataAudit hook handlers from site config.
    
    The code is mostly unchanged, other than putting the handlers in a
    class, and fixing some Phan issues and in-place changes to
    $response->message that would break an unrelated test.
    
    Bug: T430564
    Change-Id: I99731df619b67dc7bd98643197dd8aa9d5d600f8
    (cherry picked from commit d9cc39cb9687d4994c280ba01e5ce26418ab3e23)

---
+++ src/SecurityLogs/SecurityLogsHookHandler.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace MediaWiki\Extension\WikimediaCustomizations\SecurityLogs;
+
+use MediaWiki\Auth\AuthenticationResponse;
+use MediaWiki\Auth\Hook\AuthManagerLoginAuthenticateAuditHook;
+use MediaWiki\Auth\PasswordAuthenticationRequest;
+use MediaWiki\Context\RequestContext;
+use MediaWiki\Hook\GetSecurityLogContextHook;
+use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Message\Message;
+use MediaWiki\Request\WebRequest;
+use MediaWiki\SpecialPage\Hook\ChangeAuthenticationDataAuditHook;
+use MediaWiki\Status\Status;
+
+/**
+ * Various security related Logstash logging hooks:
+ * - enrich security-related log records with various traffic-related info
+ * - log information about logins, on four channels, split by success and account privileges:
+ *   goodpass, badpass, goodpass-priv, badpass-priv
+ * - log password changes for privileged accounts to badpass-priv
+ */
+class SecurityLogsHookHandler implements
+	GetSecurityLogContextHook,
+	AuthManagerLoginAuthenticateAuditHook,
+	ChangeAuthenticationDataAuditHook
+{
+
+	/** @inheritDoc */
+	public function onGetSecurityLogContext( array $info, array &$context ): void {
+		/** @var WebRequest $request */
+		$request = $info['request'];
+
+		$headers = [
+			// https://wikitech.wikimedia.org/wiki/CDN/Backend_api
+			'x-trusted-request',
+			'x-is-browser',
+			'x-ua-contact',
+			// https://gerrit.wikimedia.org/g/operations/puppet/+/production/modules/profile/files/cache/ja3n.lua
+			'x-ja3n',
+			// https://gerrit.wikimedia.org/g/operations/puppet/+/production/modules/profile/files/cache/ja4h.lua
+			'x-ja4h',
+		];
+
+		foreach ( $headers as $header ) {
+			$context[$header] = (string)$request->getHeader( $header );
+		}
+
+		// https://wikitech.wikimedia.org/wiki/X-Provenance
+		$provenance = [];
+		$provenanceString = $request->getHeader( 'X-Provenance' ) ?: '';
+		foreach ( explode( ';', $provenanceString ) as $item ) {
+			[ $label, $value ] = explode( '=', $item, 2 ) + [ 1 => '' ];
+			if ( $label !== '' ) {
+				$provenance[$label] = $value;
+			}
+		}
+
+		$context += [
+			'geocookie' => $request->getCookie( 'GeoIP', '' ),
+			'x-provenance' => $provenance,
+		];
+	}
+
+	/** @inheritDoc */
+	public function onAuthManagerLoginAuthenticateAudit( $response, $user, $username, $extraData ) {
+		$guessed = false;
+		if ( !$user && $username ) {
+			$user = MediaWikiServices::getInstance()
+				->getUserFactory()
+				->newFromName( $username );
+			$guessed = true;
+		}
+		if ( !$user || !in_array( $response->status,
+				[ AuthenticationResponse::PASS, AuthenticationResponse::FAIL ], true )
+		) {
+			return;
+		}
+
+		$context = RequestContext::getMain()->getRequest()->getSecurityLogContext( $user );
+		$privileged = $context['user_is_privileged'];
+		$successful = $response->status === AuthenticationResponse::PASS;
+		$message = $response->message ? Message::newFromSpecifier( $response->message ) : null;
+
+		$channel = $successful ? 'goodpass' : 'badpass';
+		if ( $privileged ) {
+			$channel .= '-priv';
+		}
+		$logger = LoggerFactory::getInstance( $channel );
+		$verb = $successful ? 'succeeded' : 'failed';
+
+		$logger->info( "Login $verb for {priv} {user} from {clientIp} - {ua} - {geocookie}: {messagestr}", [
+			'successful' => $successful,
+			// Backwards compatibility
+			'name' => $context['user'],
+			// Backwards compatibility
+			'clientip' => $context['clientIp'],
+			'priv' => ( $privileged ? 'elevated' : 'normal' ),
+			'guessed' => $guessed,
+			'msgname' => $message?->getKey() ?? '-',
+			'messagestr' => $message?->inLanguage( 'en' )?->text() ?? '',
+		] + $context );
+	}
+
+	/** @inheritDoc */
+	public function onChangeAuthenticationDataAudit( $req, $status ) {
+		$user = MediaWikiServices::getInstance()
+			->getUserIdentityLookup()
+			->getUserIdentityByName( $req->username );
+		$status = Status::wrap( $status );
+		if ( $req instanceof PasswordAuthenticationRequest ) {
+			$context = RequestContext::getMain()->getRequest()->getSecurityLogContext( $user );
+			$privileged = $context['user_is_privileged'];
+			if ( $privileged ) {
+				$logger = LoggerFactory::getInstance( 'badpass' );
+				$logger->info(
+					'Password change in prefs for {priv} {user}: {status} - {clientIp} - {ua} - {geocookie}',
+					[
+						// Backwards compatibility
+						'name' => $context['user'],
+						// Backwards compatibility
+						'clientip' => $context['clientIp'],
+						'priv' => 'elevated',
+						'status' => $status->isGood()
+							? 'ok'
+							: $status->getWikiText( false, false, 'en' ),
+					] + $context );
+			}
+		}
+	}
+
+}
