870e5fc3bbf71d6a4e5076ca4c452772faa0cf0e
Aaron Schulz <aschulz@wikimedia.org>
2026-07-16 18:35:43 -0700

    Fix RestSandbox title message key and import missing aliases
    Bug: T424664
    Change-Id: Ie797c3b770fd409d2a055ad8666f1937927f4007

---
+++ RestSandbox.alias.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Aliases for
+ *   Special:RestSandbox
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'RestSandbox' => [ 'RestSandbox' ],
+];
+
+/** Egyptian Arabic (مصرى) */
+$specialPageAliases['arz'] = [
+	'RestSandbox' => [ 'سبوره_رست' ],
+];
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = [
+	'RestSandbox' => [ 'ملعب_رست' ],
+];
+
+/** Persian (فارسی) */
+$specialPageAliases['fa'] = [
+	'RestSandbox' => [ 'آزمایش_رست' ],
+];
+
+/** Hebrew (עברית) */
+$specialPageAliases['he'] = [
+	'RestSandbox' => [ 'ארגז_חול_של_REST' ],
+];
+
+/** Korean (한국어) */
+$specialPageAliases['ko'] = [
+	'RestSandbox' => [ 'Rest연습장' ],
+];
+
+/** Norwegian Bokmål (norsk bokmål) */
+$specialPageAliases['nb'] = [
+	'RestSandbox' => [ 'REST-sandkasse' ],
+];
+
+/** Norwegian Nynorsk (norsk nynorsk) */
+$specialPageAliases['nn'] = [
+	'RestSandbox' => [ 'REST-sandkasse' ],
+];
+
+/** Polish (polski) */
+$specialPageAliases['pl'] = [
+	'RestSandbox' => [ 'Środowisko_testowe_REST_API' ],
+];
+
+/** Pashto (پښتو) */
+$specialPageAliases['ps'] = [
+	'RestSandbox' => [ 'ازمون_مخ_بيااوڼل' ],
+];
+
+/** Ukrainian (українська) */
+$specialPageAliases['uk'] = [
+	'RestSandbox' => [ 'REST-пісочниця' ],
+];
+
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'RestSandbox' => [ 'RestSandbox' ],
+];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'RestSandbox' => [ 'REST沙盒' ],
+];
+
+/** Traditional Chinese (中文（繁體）) */
+$specialPageAliases['zh-hant'] = [
+	'RestSandbox' => [ 'REST沙盒' ],
+];
